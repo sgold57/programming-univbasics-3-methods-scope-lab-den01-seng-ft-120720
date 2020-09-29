89f4505538f74222475f4126bad3ff33b0d01601
Sam Gold <sgold5757@gmail.com>
2020-09-29 14:40:04 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,6 +4,17 @@ def mario
 end
 
 def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
+end
+
+def link
+  catchphrase = "It's Dangerous To Go Alone! Take This."
+  puts catchphrase
+end
+
+def all_phrases
+  mario
+  toadstool
+  link
 end
